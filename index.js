d603c56b0a69d08e05b1b1e899ae594b48df7892
Parvez <pervezhossain1998@gmail.com>
2023-04-16 20:59:54 +0600

    Js Type Conversion And Coersion Added

--- index.js
+++ index.js
@@ -13,3 +13,26 @@ const firstName = "Jhonas"
 const age = 19
 
 console.log(`I am ${firstName}`)
+
+
+// Js type conversion
+
+const birthYear = "1998";
+// In Js If we Concatinate string with number the output will be a String As Js Prioritize String. So
+// We Need to convert string to a number
+const newYear = (Number(birthYear))
+console.log(newYear + 18)
+
+// Converting Number to String
+
+console.log(String(20))
+
+
+// Js Type Coersion
+console.log("I am " + 23 + " years old");
+
+let n = "1" + 1;
+n = n - 1;
+
+console.log(n);
+
