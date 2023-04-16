6f13c555c52f27542a23c728728dba5e2fac2d14
Parvez <pervezhossain1998@gmail.com>
2023-04-16 20:20:30 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const massMark = 78
+const height = 1.69
+
+console.log(massMark/ (height * height)); 
+
+
+const massJhon = 92
+const jhonHeight = 1.95
+
+console.log(massJhon / (jhonHeight * jhonHeight));
+
+const firstName = "Jhonas"
+const age = 19
+
+console.log(`I am ${firstName}`)
