116850ee6708b2af74d39bb328f11969febd9c45
Pervez <pervezhossain1998@gmail.com>
2023-04-21 11:07:28 +0600

    If Else Condinion Applied

---
+++ examplepreactice.js
@@ -0,0 +1,15 @@
+let avarageOfDolphine = ((96 + 108 + 89) / 3)
+
+console.log(avarageOfDolphine);
+
+let avarageOfKoala = ((89 + 91 + 110) / 3)
+console.log(avarageOfKoala);
+
+// Comparing Avarage
+if (avarageOfDolphine > avarageOfKoala) {
+    console.log("Winner is Dolphine");
+} else if (avarageOfDolphine < avarageOfKoala) {
+    console.log("Wineer is Koala");
+} else {
+    console.log("The Match is Draw");
+}
