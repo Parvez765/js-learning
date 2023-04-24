ffa4fe0653e7797d74e0b9f62c0f334cb9342ff5
Pervez <pervezhossain1998@gmail.com>
2023-04-24 09:33:12 +0600

    Function Added

--- script.js
+++ script.js
@@ -7,5 +7,20 @@ const passTest = true;
 if (passTest) hasDriverLicence = true
 if(hasDriverLicence) console.log("I can Drive"); // Without Stict mode output will be nothing but with strict mode it will throw and error
 
-// use strict helps us to prevent accidental error in code and helps us to fix the bug. Also it shows visible error in developer consol which makes our code more efficient to read and write. Also we can to use reserved keyword as variable which will implement in future while using strict mode. 
+// use strict helps us to prevent accidental error in code and helps us to fix the bug. Also it shows visible error in developer consol which makes our code more efficient to read and write. Also we can to use reserved keyword as variable which will implement in future while using strict mode.
 // Example : interface, private
+
+
+// FUNCTION: A function is a piece of code that we can reuse over and over in our code
+function logger() {
+    console.log("Hello From Function");
+}
+logger()
+
+function foodProcessor(apple, oranges) {
+    const juice = (`Please make ${apple} apple juice and also ${oranges} orange juice`)
+    return juice;
+}
+
+let appleJuice = foodProcessor(5, 0)
+ console.log(appleJuice); // Output: Please make 5 apple juice and also 0 orange juice
