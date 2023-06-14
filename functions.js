5a2966da92645a1b5cd0b15c1451bb2647b00d44
Pervez <pervezhossain1998@gmail.com>
2023-06-14 19:29:47 +0600

    Arrow Funtion Added

--- functions.js
+++ functions.js
@@ -8,14 +8,38 @@ function logger(name) {
 // This process is called invoking the function, running the function or calling the function
 const firstName = logger("Parvez");
 const secondName = logger("Lima") 
-console.log(firstName, secondName);
+// console.log(firstName, secondName);
 
 // Example Two
 function fruitProcessor(apple, orange) {
-    console.log(apple, orange);
+    // console.log(apple, orange);
     const juice = `I Want ${apple} Apple Juice and ${orange} Orange Juice`
     return juice
 }
 
 const juice = fruitProcessor(5, 3)
-console.log(juice);
+// console.log(juice);
+
+// Function Decleration And Expression
+
+// Function Decleration
+
+// We can initail Function Decleration before calling but can't do the same with function expression
+function calcAge1(birthYear) {
+    return 2023 - birthYear
+}
+// console.log(calcAge1(1998));
+
+
+// Function Expression
+const calcAge2 = function (birthYear) {
+    return 2023 - birthYear
+}
+// console.log(calcAge2(2000));
+
+// Arrow Function
+const birthYear = (age) => {
+    return 2023 - age
+}
+
+console.log(birthYear(1998));
